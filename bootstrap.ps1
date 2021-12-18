4ca9e5aca63f7ade265bd8cc692270da8d5248e5
Matt Radbourne <m@radbourne.net>
2021-12-18 15:18:32 +0000

    Use backslashes in bootstrap script

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,11 +1,11 @@
 $profileDir = Split-Path -parent $profile
-$windowsTerminalDir = Join-Path $home "AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"
+$windowsTerminalDir = Join-Path $home "AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 
 New-Item $profileDir -ItemType Directory -Force -ErrorAction SilentlyContinue
 
-Copy-Item -Path ./profile/** -Destination $profileDir -Include ** -Force
-Copy-Item -Path ./home/** -Destination $home -Include **
-Copy-Item -Path ./windows-terminal/settings.json -Destination $windowsTerminalDir
+Copy-Item -Path ".\profile\**" -Destination $profileDir -Include ** -Force
+Copy-Item -Path ".\home\**" -Destination $home -Include **
+Copy-Item -Path ".\windows-terminal\settings.json" -Destination $windowsTerminalDir
 
 Remove-Variable profileDir
 Remove-Variable windowsTerminalDir
