b3fdbdf8450da5a3a040972a8b1e2896be3afa27
Matt Radbourne <m@radbourne.net>
2022-01-15 13:38:36 +0000

    Add aliases

--- profile/aliases.ps1
+++ profile/aliases.ps1
@@ -1,3 +1,14 @@
-${function:l} = { Get-ChildItem }
-${function:la} = { Get-ChildItem -Force @args }
+# Shortcuts
 ${function:g} = { git @args }
+${function:h} = { Get-History @args }
+${function:j} = { Get-Job @args }
+${function:v} = { nvim @args }
+
+# List all files
+${function:l} = { Get-ChildItem @args }
+
+# List all files, including dot files
+${function:la} = { Get-ChildItem -Force @args }
+
+# List only directories
+${function:lsd} = { Get-ChildItem -Directory @args }
