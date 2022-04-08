a4248b859eb24a39e60eeaf9216a95f02a321ba0
Matt Radbourne <matt@usershaped.com>
2022-04-08 21:27:59 +0100

    Update default editor to neovim

--- profile/exports.ps1
+++ profile/exports.ps1
@@ -1,6 +1,6 @@
 # Make vim the default editor
-Set-Environment "EDITOR" "gvim --nofork"
+Set-Environment "EDITOR" "nvim"
 Set-Environment "GIT_EDITOR" $Env:EDITOR
 
 # Disable the Progress Bar
-$ProgressPreference='SilentlyContinue'
+$ProgressPreference = 'SilentlyContinue'
