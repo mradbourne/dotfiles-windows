bf87ffff43e262b373d4bf61b7d654f965e3f4ca
Matt Radbourne <matt@usershaped.com>
2022-04-09 16:08:49 +0100

    Remove some colouring from posh-git prompt

--- profile/components/git.ps1
+++ profile/components/git.ps1
@@ -1,3 +1,23 @@
-if (((Get-Command git -ErrorAction SilentlyContinue) -ne $null) -and ((Get-Module -ListAvailable Posh-Git -ErrorAction SilentlyContinue) -ne $null)) {
+﻿if (((Get-Command git -ErrorAction SilentlyContinue) -ne $null) -and ((Get-Module -ListAvailable Posh-Git -ErrorAction SilentlyContinue) -ne $null)) {
   Import-Module Posh-Git
 }
+
+# Override posh-git settings
+$s = $global:GitPromptSettings
+$s.PathStatusSeparator = " "
+$s.BeforeStatus = "["
+$s.DelimStatus = " |"
+$s.AfterStatus = "]"
+$s.BeforePath = ""
+$s.AfterPath = ""
+$s.BeforeIndex = ""
+$s.BeforeStash = " ("
+$s.AfterStash = ")"
+$s.LocalDefaultStatusSymbol = ""
+$s.LocalWorkingStatusSymbol = "!"
+$s.LocalStagedStatusSymbol = "~"
+$s.BranchGoneStatusSymbol = "×"
+$s.BranchIdenticalStatusSymbol = "≡"
+$s.BranchAheadStatusSymbol = "↑"
+$s.BranchBehindStatusSymbol = "↓"
+$s.BranchBehindAndAheadStatusSymbol = "↕"
